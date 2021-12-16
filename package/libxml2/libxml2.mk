63b33c7d3888483ba1b84d7d709b3a579e5335ac
Poligraf <>
2021-12-17 06:47:00 +1300

    libxml2 python2 fix

--- package/libxml2/libxml2.mk
+++ package/libxml2/libxml2.mk
@@ -4,38 +4,43 @@
 #
 ################################################################################
 
-LIBXML2_VERSION = 2.9.12
-LIBXML2_SITE = http://xmlsoft.org/sources
+LIBXML2_VERSION = 2.9.1
+LIBXML2_SITE = ftp://xmlsoft.org/libxml2
 LIBXML2_INSTALL_STAGING = YES
+LIBXML2_AUTORECONF = YES
 LIBXML2_LICENSE = MIT
 LIBXML2_LICENSE_FILES = COPYING
-LIBXML2_CPE_ID_VENDOR = xmlsoft
 LIBXML2_CONFIG_SCRIPTS = xml2-config
 
-# relocation truncated to fit: R_68K_GOT16O
-ifeq ($(BR2_m68k_cf),y)
-LIBXML2_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -mxgot"
+ifneq ($(BR2_LARGEFILE),y)
+LIBXML2_CONF_ENV = CC="$(TARGET_CC) $(TARGET_CFLAGS) -DNO_LARGEFILE_SOURCE"
 endif
 
-LIBXML2_CONF_OPTS = --with-gnu-ld --without-python --without-debug
+LIBXML2_CONF_OPTS = --with-gnu-ld --without-python --without-debug --without-lzma
 
 HOST_LIBXML2_DEPENDENCIES = host-pkgconf
-LIBXML2_DEPENDENCIES = host-pkgconf
 
-HOST_LIBXML2_CONF_OPTS = --without-zlib --without-lzma --without-python
+HOST_LIBXML2_CONF_OPTS = --without-zlib --without-lzma
+
+# mesa3d uses functions that are only available with debug
+ifeq ($(BR2_PACKAGE_MESA3D),y)
+HOST_LIBXML2_CONF_OPTS += --with-debug
+else
+HOST_LIBXML2_CONF_OPTS += --without-debug
+endif
+
+ifeq ($(BR2_PACKAGE_HOST_LIBXML2_PYTHON),y)
+HOST_LIBXML2_DEPENDENCIES += host-python
+HOST_LIBXML2_CONF_OPTS += --with-python=$(HOST_DIR)/usr
+else
+HOST_LIBXML2_CONF_OPTS += --without-python
+endif
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
 LIBXML2_DEPENDENCIES += zlib
-LIBXML2_CONF_OPTS += --with-zlib=$(STAGING_DIR)/usr
+LIBXML2_CONF_OPTS += --with-zlib
 else
 LIBXML2_CONF_OPTS += --without-zlib
-endif
-
-ifeq ($(BR2_PACKAGE_XZ),y)
-LIBXML2_DEPENDENCIES += xz
-LIBXML2_CONF_OPTS += --with-lzma
-else
-LIBXML2_CONF_OPTS += --without-lzma
 endif
 
 LIBXML2_DEPENDENCIES += $(if $(BR2_PACKAGE_LIBICONV),libiconv)
@@ -46,13 +51,8 @@ else
 LIBXML2_CONF_OPTS += --without-iconv
 endif
 
-define LIBXML2_CLEANUP_XML2CONF
-	rm -f $(TARGET_DIR)/usr/lib/xml2Conf.sh
-endef
-LIBXML2_POST_INSTALL_TARGET_HOOKS += LIBXML2_CLEANUP_XML2CONF
-
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
 
 # libxml2 for the host
-LIBXML2_HOST_BINARY = $(HOST_DIR)/bin/xmllint
+LIBXML2_HOST_BINARY = $(HOST_DIR)/usr/bin/xmllint
