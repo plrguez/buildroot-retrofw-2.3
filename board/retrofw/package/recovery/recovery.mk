14c9aaa43584d0034a4d8fed337b863bdc323e66
Poligraf <>
2021-12-21 21:26:59 +1300

    add prg's recovery

--- board/retrofw/package/recovery/recovery.mk
+++ board/retrofw/package/recovery/recovery.mk
@@ -3,8 +3,9 @@
 # recovery
 #
 ################################################################################
-RECOVERY_VERSION = 58dcc75
-RECOVERY_SITE = https://github.com/retrofw/recovery.git
+RECOVERY_VERSION = master
+#RECOVERY_SITE = $(call github,retrofw,recovery,$(RECOVERY_VERSION))
+RECOVERY_SITE = https://github.com/retrofw/recovery
 RECOVERY_SITE_METHOD = git
 
 RECOVERY_DEPENDENCIES = sdl sdl_image sdl_ttf
